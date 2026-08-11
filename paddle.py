7a1246979aedc5492a774dfbccc976d8ecb2bda6
Sunny <nayakamouryateja@gmail.com>
2026-08-11 12:17:06 +0530

    added default speed global in paddle.py

--- paddle.py
+++ paddle.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
 UPPER_BOUND =  250
 LOWER_BOUND = -250
+PADDLE_DEFAULT_SPEED = 15
 class Paddle(Turtle):
     def __init__(self,x,y):
         self.x = x
@@ -13,7 +14,7 @@ class Paddle(Turtle):
         self.goto(self.x,self.y)
     def up(self):
         if self.ycor() <= UPPER_BOUND:
-            self.sety(self.ycor()+15)
+            self.sety(self.ycor()+ PADDLE_DEFAULT_SPEED)
     def down(self):
         if self.ycor() >= LOWER_BOUND:
-            self.sety(self.ycor() - 15)
+            self.sety(self.ycor() - PADDLE_DEFAULT_SPEED)
