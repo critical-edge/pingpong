4287ba004dd70dfd3b98f68cb6a2050a075def49
Sunny <nayakamouryateja@gmail.com>
2026-08-11 12:14:26 +0530

    added boundries values and set up boundry condtions in paddle.py

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 from turtle import Screen
-
+from paddle import Paddle
+import time
+KEYS = {
+    "up"  : False,
+    "down": False,
+        }
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
+REFRESH_RATE = 0.02
+GAME = True
 
 screen = Screen()
 screen.setup(width= SCREEN_WIDTH,height=SCREEN_HEIGHT)
 screen.bgcolor("black")
 screen.title("Ping Pong")
 screen.tracer(0)
+pad = Paddle(-390,0)
+
+screen.listen()
+screen.onkeypress(key="Up",   fun=lambda: KEYS.update(up = True))
+screen.onkeyrelease(key="Up", fun=lambda: KEYS.update(up = False))
+screen.onkeypress(key="Down", fun= lambda: KEYS.update(down = True))
+screen.onkeyrelease(key="Down", fun=lambda: KEYS.update(down = False))
+
+while GAME:
+    time.sleep(REFRESH_RATE)
+    screen.update()
+    if KEYS["up"]:
+        pad.up()
+
+    if KEYS["down"]:
+        pad.down()
 screen.exitonclick()
---
+++ paddle.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+UPPER_BOUND =  250
+LOWER_BOUND = -250
+class Paddle(Turtle):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        super().__init__()
+        self.shape("square")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.color("white")
+        self.penup()
+        self.goto(self.x,self.y)
+    def up(self):
+        if self.ycor() <= UPPER_BOUND:
+            self.sety(self.ycor()+15)
+    def down(self):
+        if self.ycor() >= LOWER_BOUND:
+            self.sety(self.ycor() - 15)
